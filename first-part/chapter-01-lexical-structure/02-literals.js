ab0d57534d5ce9e8ded49454067c8b6c05560d1d
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-03-14 19:18:30 +0100

    chore: write a script listing some literals values javascript use

---
+++ first-part/chapter-01-lexical-structure/02-literals.js
@@ -0,0 +1,10 @@
+
+12              // literal integer number
+1.2             // literal float number
+
+"hello world"   // literal text string using double quote
+'hi'            // literal text string using single quote
+true            // literal true boolean
+false           // literal false boolean
+/javascript/gi  // literal regular expression
+null            // literal null value
