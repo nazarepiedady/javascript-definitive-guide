8d7b4207d9370e82b5fe9611420428b82de73d9b
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-03-26 10:41:32 +0100

    chore: list in comment form all keywords used before es5

---
+++ first-part/chapter-01-lexical-structure/03-reserved-words.js
@@ -0,0 +1,15 @@
+// reserved words
+
+
+// keywords before ECMAScript 5
+
+/*
+
+break      delete   function    return   typeof
+case       do       if          switch   var
+catch      else     in          this     void
+continue   false    instanceof  throw    while
+debugger   finally  new         true     with
+default    for      null        try
+
+*/
