c887fc5e208544d13cba6725f7c28dda4dfe8b85
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-17 14:17:36 +0100

    chore: create a variable and assign it a deprecated version of octal integer

--- first-part/chapter-02-types-values-variables/01-type-number.js
+++ first-part/chapter-02-types-values-variables/01-type-number.js
@@ -3,3 +3,6 @@ var decimalInteger = 12;
 
 // integer with 16 or hexadecimal base
 var hexadecimalInteger = 0xff; // 255
+
+// integer with 8 or octal base (deprecated)
+var deprecatedOctalInteger = 0337 // 255
