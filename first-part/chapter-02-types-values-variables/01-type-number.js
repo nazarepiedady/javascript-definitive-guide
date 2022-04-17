4eb6f18fdf176f10d87e62eea691d80605299e10
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-17 14:25:14 +0100

    chore: create a float number variable using exponetional notation

--- first-part/chapter-02-types-values-variables/01-type-number.js
+++ first-part/chapter-02-types-values-variables/01-type-number.js
@@ -12,3 +12,6 @@ var standardOctalInteger = 0o377 // 255
 
 // float in decimal base
 var decimalFloat = 12.24
+
+// float with exponetional notation
+var decimalFloatExponetional = 6.02e23 // 6.02 x 10 ^ 23
