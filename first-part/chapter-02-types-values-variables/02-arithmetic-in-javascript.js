a80df7eeb2d0d5863b6e0ccf2e79e86531a55337
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-08 12:25:42 +0100

    chore: write an example of arithmetical operations

---
+++ first-part/chapter-02-types-values-variables/02-arithmetic-in-javascript.js
@@ -0,0 +1,22 @@
+// addition
+var sum = 1 + 1;
+
+// subtration
+var sub = 2 - 1;
+
+// multiplication
+var mul = 3 * 2;
+
+// division
+var div = 6 / 2;
+
+// module
+var mod = 8 % 2;
+
+// every results
+var results = [ sum, sub, mul, div, mod ];
+
+// print each result
+results.forEach(function show(result) {
+  console.log(result);
+});
