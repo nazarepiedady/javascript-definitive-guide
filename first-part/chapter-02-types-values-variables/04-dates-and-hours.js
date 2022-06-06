8c9c8619ca65e96d6c66f9ce2cd7891492a57d43
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-06 19:01:02 +0100

    chore: use `new Date()` to generate current time

--- first-part/chapter-02-types-values-variables/04-dates-and-hours.js
+++ first-part/chapter-02-types-values-variables/04-dates-and-hours.js
@@ -1,2 +1,4 @@
 var then = new Date(2010, 0, 1); // 2010/01/01 [YYYY, MM, DD]
 var later = new Date(2010, 0, 1, 17, 10, 30); // 2010/01/01, 05:10:30 PM
+
+var now = new Date(); // The current time and date
