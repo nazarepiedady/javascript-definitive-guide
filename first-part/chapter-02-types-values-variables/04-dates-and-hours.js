d414bde0c1648db03ae6b8e30b426cc1279de5ce
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-06 19:22:47 +0100

    chore: show up some usage of the date object

--- first-part/chapter-02-types-values-variables/04-dates-and-hours.js
+++ first-part/chapter-02-types-values-variables/04-dates-and-hours.js
@@ -1,7 +1,33 @@
-var then = new Date(2010, 0, 1); // 2010/01/01 [YYYY, MM, DD]
+var then = new Date(2010, 0, 1);              // 2010/01/01 [YYYY, MM, DD]
 var later = new Date(2010, 0, 1, 17, 10, 30); // 2010/01/01, 05:10:30 PM
 
-var now = new Date(); // The current time and date
+var now = new Date();     // The current time and date
 var elapsed = now - then; // subtract de date: interval in milliseconds
 
-var laterFullYear = later.getFullYear(); // 2010
+var laterFullYear = later.getFullYear();    // 2010
+var laterMonth = later.getMonth();          // 0 >> Janery
+var laterDate = later.getDate();            // 1
+var laterDay = later.getDay();              // 5
+var laterHours = later.getHours();          // 17
+var laterUTCHours = later.getUTCHours();    // Hours in UTC; depend on timezone
+var laterDateString = later.toString();     //
+var laterDateUTCString = later.toUTCString(); //
+var laterLocaleDateString = later.toLocaleDateString();
+var laterLocaleTimeString = later.toLocaleTimeString();
+var laterISOString = later.toISOString();
+
+[
+  laterFullYear,
+  laterMonth,
+  laterDate,
+  laterDay,
+  laterHours,
+  laterUTCHours,
+  laterDateString,
+  laterDateUTCString,
+  laterLocaleDateString,
+  laterLocaleTimeString,
+  laterISOString
+].forEach(function(item) {
+  console.log(item);
+});
